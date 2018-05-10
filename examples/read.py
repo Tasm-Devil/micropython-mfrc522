6646b3e309170f8fd0e1e5bf6bcd031c526ad6d0
Pascal Deneaux <deneaux@web.de>
2018-05-10 17:54:35 +0200

    Update read.py

--- examples/read.py
+++ examples/read.py
@@ -1,46 +1,27 @@
-import mfrc522
-from os import uname
+from time import sleep_ms
 
+# SPI-BUS INIT
+from machine import Pin, SPI
+sck = Pin(18, Pin.OUT)
+mosi = Pin(23, Pin.OUT)
+miso = Pin(19, Pin.OUT)
+spi = SPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
 
-def do_read():
+# Testing rfid module
+# https://github.com/wendlers/micropython-mfrc522
+from lib.rfid.mfrc522 import MFRC522
+sda = Pin(5, Pin.OUT)
 
-	if uname()[0] == 'WiPy':
-		rdr = mfrc522.MFRC522("GP14", "GP16", "GP15", "GP22", "GP17")
-	elif uname()[0] == 'esp8266':
-		rdr = mfrc522.MFRC522(0, 2, 4, 5, 14)
-	else:
-		raise RuntimeError("Unsupported platform")
-
-	print("")
-	print("Place card before reader to read from address 0x08")
-	print("")
-
-	try:
-		while True:
-
-			(stat, tag_type) = rdr.request(rdr.REQIDL)
-
-			if stat == rdr.OK:
-
-				(stat, raw_uid) = rdr.anticoll()
-
-				if stat == rdr.OK:
-					print("New card detected")
-					print("  - tag type: 0x%02x" % tag_type)
-					print("  - uid	 : 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
-					print("")
-
-					if rdr.select_tag(raw_uid) == rdr.OK:
-
-						key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-						if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
-							print("Address 8 data: %s" % rdr.read(8))
-							rdr.stop_crypto1()
-						else:
-							print("Authentication error")
-					else:
-						print("Failed to select tag")
-
-	except KeyboardInterrupt:
-		print("Bye")
+try:
+    while True:
+        rdr = MFRC522(spi, sda)
+        uid = ""
+        (stat, tag_type) = rdr.request(rdr.REQIDL)
+        if stat == rdr.OK:
+            (stat, raw_uid) = rdr.anticoll()
+            if stat == rdr.OK:
+                uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+                print(uid)
+                sleep_ms(100)
+except KeyboardInterrupt:
+    print("Bye")
