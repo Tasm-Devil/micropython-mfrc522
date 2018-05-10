b3e7464a6b18cfd4452a164db599f0ef81fd64f0
Pascal Deneaux <deneaux@web.de>
2018-05-10 17:57:40 +0200

    Update read.py

--- examples/read.py
+++ examples/read.py
@@ -9,16 +9,17 @@ spi = SPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
 
 sda = Pin(5, Pin.OUT)
 
-try:
-    while True:
-        rdr = MFRC522(spi, sda)
-        uid = ""
-        (stat, tag_type) = rdr.request(rdr.REQIDL)
-        if stat == rdr.OK:
-            (stat, raw_uid) = rdr.anticoll()
+def do_read():
+    try:
+        while True:
+            rdr = MFRC522(spi, sda)
+            uid = ""
+            (stat, tag_type) = rdr.request(rdr.REQIDL)
             if stat == rdr.OK:
-                uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
-                print(uid)
-                sleep_ms(100)
-except KeyboardInterrupt:
-    print("Bye")
+                (stat, raw_uid) = rdr.anticoll()
+                if stat == rdr.OK:
+                    uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+                    print(uid)
+                    sleep_ms(100)
+    except KeyboardInterrupt:
+        print("Bye")
